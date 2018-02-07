ae8695df60222ab4bb8b391b7dbefa5d1d44bf30
fanling123 <36067794+fanling123@users.noreply.github.com>
2018-02-07 11:13:50 +0800

    test

--- src/main/java/com/beimi/web/handler/api/rest/user/JoinAndLeaveRoomController.java
+++ src/main/java/com/beimi/web/handler/api/rest/user/JoinAndLeaveRoomController.java
@@ -41,8 +41,9 @@ public class JoinAndLeaveRoomController {
                 }
                     resu=new ResultData(message,"200" );
             }
+            else {
                 resu = new ResultData(MessageEnum.USER_TOKEN, "201");
-
+            }
         }else{
             resu=new ResultData("参数为空","203" );
         }
